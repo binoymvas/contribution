b38d3abb5d4fd2815ac0f7f7a82547ae9566a273
Igor V. Dyukov <div@nephoscale.com>
2016-01-26 15:45:43 +0000

    prettify Chef table layout

--- openstack_dashboard/dashboards/inventory/chef/tables.py
+++ openstack_dashboard/dashboards/inventory/chef/tables.py
@@ -8,11 +8,23 @@ class ChefTable(tables.DataTable):
     id = tables.Column('id',
         link = "horizon:inventory:chef:detail",
         verbose_name = _('Node Name'),
+        cell_attributes_getter = lambda v: {"nowrap": True},
     )
-    platform = tables.Column('platform', verbose_name = _('Platform'),)
-    #fqdn = tables.Column('fqdn', verbose_name = _('Node FQDN'),)
-    ipaddr = tables.Column('ipaddr', verbose_name = _('IP Address'),)
-    uptime = tables.Column('uptime', verbose_name = _('Uptime'),)
-    lstchk = tables.Column('lstchk', verbose_name = _('Last Check-In'),)
+    platform = tables.Column('platform',
+        verbose_name = _('Platform'),
+        cell_attributes_getter = lambda v: {"nowrap": True},
+    )
+    ipaddr = tables.Column('ipaddr',
+        verbose_name = _('IP Address'),
+        cell_attributes_getter = lambda v: {"nowrap": True},
+    )
+    uptime = tables.Column('uptime',
+        verbose_name = _('Uptime'),
+        cell_attributes_getter = lambda v: {"nowrap": True},
+    )
+    lstchk = tables.Column('lstchk',
+        verbose_name = _('Last Check-In'),
+        cell_attributes_getter = lambda v: {"nowrap": True},
+    )
     roles = tables.Column('roles', verbose_name = _('Roles'),)
 
